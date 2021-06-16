a5d99223742d910f6fb2fa1ee809a6ac7fd8c605
robert-brelinski <robertbrelinski@gmail.com>
2021-06-16 08:22:05 +0200

    Clicker Added

---
+++ Fake3dGame/Assets/MyScripts/Clicker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    Renderer rend;
+    Material mat;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    }
+}
